878f4f635108da2b234235656b17ea317d54511b
Alan Santos <alansods@gmail.com>
2022-02-03 17:55:08 -0300

    escondi frase

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -37,7 +37,7 @@ export default function Home() {
       <div className="logo">
       <lottie-player style={{width: "220px", height: "220px", "border-radius": "50%", background: "#fff", padding: "15px", "margin-bottom": "15px"}} src="https://assets8.lottiefiles.com/packages/lf20_fzpss2yc.json"  background="transparent" speed="1" loop  autoplay></lottie-player>
         <h1>Encurtador de Links</h1>
-        <span>Cole seu link aqui 👇</span>
+        {/* <span>Cole seu link aqui 👇</span> */}
       </div>
 
       <div className="area-input">
